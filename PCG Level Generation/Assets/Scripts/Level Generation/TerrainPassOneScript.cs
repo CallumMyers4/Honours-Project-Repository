80fc13727d6e92f66907d23aa2b958e713208c4d
CallumMyers4 <callummyers4@gmail.com>
2024-11-12 12:26:09 +0000

    Added a starting platform check
    Can now edit the length of the start platform which will create flat ground at y = 1 to prevent player from spawning in gaps/mountains
    Re-enabled player's collider and fixed rotation issue
    
    Major bug:
    Player jump doesn't work properly, fix ASAP

--- PCG Level Generation/Assets/Scripts/Level Generation/TerrainPassOneScript.cs
+++ PCG Level Generation/Assets/Scripts/Level Generation/TerrainPassOneScript.cs
@@ -9,7 +9,7 @@ public class TerrainPassOneScript : MonoBehaviour
     [SerializeField]
     private float lowestY, highestY;    //top and bottom constraints of the level height
     [SerializeField]
-    private int maxHeightChange;
+    private int maxHeightChange, startPlatformLength;
     [SerializeField]
     private PerlinNoiseGeneratorScript noiseGenerator;  //reference to script for generating noise
     [SerializeField]
@@ -33,24 +33,34 @@ public class TerrainPassOneScript : MonoBehaviour
         //run through each x position in the level
         for (int x = 0; x < levelWidth; x++)
         {
-            int textureX = Mathf.FloorToInt((float)(x + startX) / levelWidth * noiseTexture.width); //moves to the correct pixel within the texture
-            Color pixelColor = noiseTexture.GetPixel(textureX, 0); //gets the noise value at this position (0 means it goes in a straight
-                                                                    //horiontal line through the texture)
+            int groundHeight;
 
-            float noiseValue = pixelColor.grayscale;    //gives a value between 0 and 1
-            float groundDirection = (noiseValue * 2) - 1;   //scales to between -1 and 1 to allow for going above or below 0
+            //sets ground height to 0 until a flat starting platform has been generated (prevents player spawning inside a tile)
+            if (x < startPlatformLength)
+            {
+                groundHeight = 1;
+            }
+            else
+            {
+                int textureX = Mathf.FloorToInt((float)(x + startX) / levelWidth * noiseTexture.width); //moves to the correct pixel within the texture
+                Color pixelColor = noiseTexture.GetPixel(textureX, 0); //gets the noise value at this position (0 means it goes in a straight
+                                                                        //horiontal line through the texture)
 
-            /*converts to an int, then ensures a value of -1 would map to the lowest Y value constraint and a value of 1 would map
-              to the highest Y constraint
-              (highestY - lowestY)/2 scales value
-              (highestY + lowestY)/2 centres it back to 0
-            */
-            int groundHeight = Mathf.FloorToInt(groundDirection * ((highestY - lowestY) / 2) + ((highestY + lowestY) / 2));
-            groundHeight = Mathf.Max(groundHeight, 1); //always place at least one block
+                float noiseValue = pixelColor.grayscale;    //gives a value between 0 and 1
+                float groundDirection = (noiseValue * 2) - 1;   //scales to between -1 and 1 to allow for going above or below 0
 
-            //check to ensure the new height does not make the level unplayable, then sets last height to this one for next loop
-            groundHeight = Mathf.Clamp(groundHeight, previousHeight - maxHeightChange, previousHeight + maxHeightChange);
-            previousHeight = groundHeight;
+                /*converts to an int, then ensures a value of -1 would map to the lowest Y value constraint and a value of 1 would map
+                to the highest Y constraint
+                (highestY - lowestY)/2 scales value
+                (highestY + lowestY)/2 centres it back to 0
+                */
+                groundHeight = Mathf.FloorToInt(groundDirection * ((highestY - lowestY) / 2) + ((highestY + lowestY) / 2));
+                groundHeight = Mathf.Max(groundHeight, 1); //always place at least one block
+
+                //check to ensure the new height does not make the level unplayable, then sets last height to this one for next loop
+                groundHeight = Mathf.Clamp(groundHeight, previousHeight - maxHeightChange, previousHeight + maxHeightChange);
+                previousHeight = groundHeight;
+            }
 
             //places blocks up to the correct ground height for the current X position
             for (int y = 0; y < groundHeight; y++)
