4947c6d971b9e964d3996834aaab945a307749b4
CallumMyers4 <callummyers4@gmail.com>
2024-12-19 13:25:16 +0000

    Fixed some errors in AdjustValues()
    Now calculates flat blocks correctly
    Prints a debug showing the current chance based on Y

--- PCG Level Generation/Assets/Scripts/Level Generation/TerrainPassTwoScript.cs
+++ PCG Level Generation/Assets/Scripts/Level Generation/TerrainPassTwoScript.cs
@@ -14,7 +14,7 @@ public class TerrainPassTwoScript : MonoBehaviour
 
     //number of gaps been placed in a row, gap length limit, position of last gap, blocks since this position, blocks at
     //the same Y as block being checked
-    private int consecutiveGaps = 0, lastGapX = 0, blocksSinceGap = 0, blocksAtCurY;
+    private int consecutiveGaps = 0, gapsLimit = 4, lastGapX = 0, blocksSinceGap = 0, blocksAtCurY;
     
     //keep track of current state being checked
     private enum groundStates
@@ -58,26 +58,6 @@ public class TerrainPassTwoScript : MonoBehaviour
         //run through each position in the level (starting at the end of the initial platform, ending at the end of the level)
         for (int i = firstPass.startPlatformLength; i < firstPass.endX; i++)
         {
-            //temp - use Markov Chains later
-            int randChoice = UnityEngine.Random.Range(0, 3);
-            if (randChoice == 1)
-            {
-                /*this fires a raycast to find all objects at the curernt X position in the loop, starting from just above the highest
-                possible Y position set in firstPass, uses vector2.down to fire directly downwards, at a length of just over the
-                difference between highest and lowest Y to make sure it doesnt miss any objects, and returns all of these objects it
-                finds as a struct*/
-                RaycastHit2D[] objectsAtX = Physics2D.RaycastAll(new Vector2(i, firstPass.highestY + 1), Vector2.down, 
-                                                                (MathF.Abs(firstPass.highestY - firstPass.lowestY) + 2));
-
-                //runs a loop to go through all objects stored in the struct to delete them
-                for (int j = 0; j < objectsAtX.Length; j++)
-                {
-                    Destroy(objectsAtX[j].collider.gameObject);     //uses the collider component of each object found 
-                                                                    //to access the gameObject and destroys it
-                }
-            }
-
-
             //start of Markovs
             AdjustGroundValues(i);  //pass in the current X value being checked to allow the Markov to change values
 
@@ -98,6 +78,30 @@ public class TerrainPassTwoScript : MonoBehaviour
                 }
 
                 lastGapX = i;   //set the X position of the last gap to be here
+                consecutiveGaps++;  //add 1 to consecutive counter
+            }
+            else
+                consecutiveGaps = 0;   //reset counter
+
+
+
+            //temp - use Markov Chains later
+            int randChoice = UnityEngine.Random.Range(0, 3);
+            if (randChoice == 1)
+            {
+                /*this fires a raycast to find all objects at the curernt X position in the loop, starting from just above the highest
+                possible Y position set in firstPass, uses vector2.down to fire directly downwards, at a length of just over the
+                difference between highest and lowest Y to make sure it doesnt miss any objects, and returns all of these objects it
+                finds as a struct*/
+                RaycastHit2D[] objectsAtX = Physics2D.RaycastAll(new Vector2(i, firstPass.highestY + 1), Vector2.down, 
+                                                                (MathF.Abs(firstPass.highestY - firstPass.lowestY) + 2));
+
+                //runs a loop to go through all objects stored in the struct to delete them
+                for (int j = 0; j < objectsAtX.Length; j++)
+                {
+                    Destroy(objectsAtX[j].collider.gameObject);     //uses the collider component of each object found 
+                                                                    //to access the gameObject and destroys it
+                }
             }
         }
     }
@@ -119,23 +123,32 @@ public class TerrainPassTwoScript : MonoBehaviour
 
     //set blocks since gap, blocks at Y, etc.
     private void AdjustGroundValues(int currentX)
-    {        
+    {     
+        blocksAtCurY = 0;   //reset counter each time a new tile is being checked
+
         //fires a raycast to get the ground at current X position (using Raycast instead of RaycastAll because we only need the
         //first object hit since this will be the main ground)        
         RaycastHit2D currentGround = Physics2D.Raycast(new Vector2(currentX, firstPass.highestY + 1), Vector2.down, 
                                                                 (MathF.Abs(firstPass.highestY - firstPass.lowestY) + 2));
 
-        //store the Y value of the ground
-        float currentY = currentGround.collider.gameObject.transform.position.y;
+        float currentY = currentGround.collider.gameObject.transform.position.y;    //store the Y value of the ground
+
+        float checkGroundY = 10000; //y pos of ground to be checked (start at high number so it does not accidentally add a +1 when there
+                                    //is no ground)
 
         //runs over all the positions looking backwards from current X
         for (int i = currentX; i > currentX - positionsToCheckBack; i--)
         {  
             //do the same as above but for the ground positions which are to be checked
-            RaycastHit2D checkGround = Physics2D.Raycast(new Vector2(currentX, firstPass.highestY + 1), Vector2.down, 
+            RaycastHit2D checkGround = Physics2D.Raycast(new Vector2(i, firstPass.highestY + 1), Vector2.down, 
                                                                     (MathF.Abs(firstPass.highestY - firstPass.lowestY) + 2));
-            float checkGroundY = checkGround.collider.gameObject.transform.position.y;
 
+            //if the ground exists, compare Y otherwise end search because it must be a gap (different Y)
+            if (checkGround.collider != null)                                                        
+                checkGroundY = checkGround.collider.gameObject.transform.position.y;
+            else
+                continue;
+            
             //if they are the same Y level then add 1 to the flat ground counter
             if (checkGroundY == currentY)
                blocksAtCurY++;
@@ -146,17 +159,29 @@ public class TerrainPassTwoScript : MonoBehaviour
         //runs over all the positions looking forwards from current X
         for (int i = currentX; i < currentX + positionsToCheckForward; i++)
         {  
-            //again exactly same as looking backwards just moving in the other direction
-            RaycastHit2D checkGround = Physics2D.Raycast(new Vector2(currentX, firstPass.highestY + 1), Vector2.down, 
+            //do the same as above but for the ground positions which are to be checked
+            RaycastHit2D checkGround = Physics2D.Raycast(new Vector2(i, firstPass.highestY + 1), Vector2.down, 
                                                                     (MathF.Abs(firstPass.highestY - firstPass.lowestY) + 2));
-            float checkGroundY = checkGround.collider.gameObject.transform.position.y;
 
+            //if the ground exists, compare Y otherwise end search because it must be a gap (different Y)
+            if (checkGround.collider != null)                                                        
+                checkGroundY = checkGround.collider.gameObject.transform.position.y;
+            else
+                continue;
+            
+            //if they are the same Y level then add 1 to the flat ground counter
             if (checkGroundY == currentY)
                blocksAtCurY++;
             else
-                continue;
+                continue;   //if not then end the search since this is as far back as the flat ground goes
         }
 
+        int chance = 50;
+        for (int i = 0; i < blocksAtCurY; i++)
+        {
+            chance += 10;
+        }
+        Debug.Log($"FlatBlocks: {blocksAtCurY} | Chance:  {chance} | CurrentX: {currentX}| YToCheck: {currentY}");
 
         blocksSinceGap = currentX - lastGapX;   //simple calculation to find the last gap
     }
