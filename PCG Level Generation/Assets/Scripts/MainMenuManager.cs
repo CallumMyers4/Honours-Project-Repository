bc60643d99e581d9910819206178c948db261b92
CallumMyers4 <callummyers4@gmail.com>
2025-02-07 11:03:13 +0000

    Finished menu
    Added button functionality
     --Notes--
    Remember to update scene var in Play button when new passes are built

--- PCG Level Generation/Assets/Scripts/MainMenuManager.cs
+++ PCG Level Generation/Assets/Scripts/MainMenuManager.cs
@@ -1,14 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField]
-    private GameObject controlsPanel;
-    
+    private GameObject controlsPanel, mainPanel;
+    [SerializeField]
+    private string mainLevel;
+ 
     public void EnableControls()
     {
         controlsPanel.SetActive(true);
+        mainPanel.SetActive(false);
+    }
+
+    public void DisableControls()
+    {
+        controlsPanel.SetActive(false);
+        mainPanel.SetActive(true);
+    }
+
+    public void StartLevels()
+    {
+        SceneManager.LoadScene(mainLevel);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 }
