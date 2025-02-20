5044ac0e35e3e69ba67311c88cd27008c554fc4a
CallumMyers4 <callummyers4@gmail.com>
2025-02-20 12:10:36 +0000

    Changed gaps and platforms slider values to work as multiplier

--- PCG Level Generation/Assets/Scripts/GeneralLevelManagerScript.cs
+++ PCG Level Generation/Assets/Scripts/GeneralLevelManagerScript.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class GeneralLevelManagerScript : MonoBehaviour
@@ -10,6 +11,8 @@ public class GeneralLevelManagerScript : MonoBehaviour
     public TMP_Text coinsCounterText;
     public PlayerMovementScript player;
     public CollectablesPassFourScript collectables;
+
+    public Slider lengthSlider, hillsSlider, gapsSlider, platformsSlider, enemiesSlider, coinsSlider;   //references to UI parameter sliders
     // Start is called before the first frame update
     void Start()
     {
@@ -40,5 +43,15 @@ public class GeneralLevelManagerScript : MonoBehaviour
     {
         winPanel.SetActive(false);
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void GenerateLevel()
+    {
+        Debug.Log("Length: " + lengthSlider.value);
+        Debug.Log("Hills: " + hillsSlider.value);
+        Debug.Log("Gaps: " + gapsSlider.value);
+        Debug.Log("Platforms: " + platformsSlider.value);
+        Debug.Log("Enemies: " + enemiesSlider.value);
+        Debug.Log("Coins: " + coinsSlider.value);
     }
 }
